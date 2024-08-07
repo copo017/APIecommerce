7cdd37f2c56a2cfd0728724478debb2ccc7bcfda
copo <sebastianorellana017@gmail.com>
2024-08-07 03:28:26 -0400

    up 4

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,11 +85,15 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse("postgresql://product_sql_ddrl_user:pEPFmaoHHwTXtRkI5XVFG7cX0mYZpqEe@dpg-cqphtpdsvqrc73frtepg-a.oregon-postgres.render.com/product_sql_ddrl")
 }
 
 # python manage.py dumpdata > db.json
